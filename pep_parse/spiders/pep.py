4331cc17457fd732c56b02a935d95435f42f6fb5
Iskander <logick7@mail.ru>
2023-07-24 23:42:25 +0300

    4

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -5,7 +5,6 @@ from pep_parse.items import PepParseItem
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
-    allowed_domains = ['peps.python.org']
     start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
